6fc06ca8554cd4da9434f90a86a57c9b65602158
Brian Schlining <bschlining@gmail.com>
2020-01-09 20:43:41 -0800

    Adding documentation

--- scilube-core/src/main/scala/scilube/package.scala
+++ scilube-core/src/main/scala/scilube/package.scala
@@ -2,24 +2,24 @@ import java.text.SimpleDateFormat
 import java.util.{Date, TimeZone}
 
 /**
- * '''scilube-core''' - Science libraries for Scala.
- *
- * The scilube-core package contains the base math functions for Scilube. These functions are loosely
- * modeled after [[http://www.mathworks.com/products/matlab/ Matlab's]] functions. Why Matlab? For
- * many science applications, the code is prototyped in Matlab and then ported to other languages
- * for efficiency reasons or to satisfy deployment requirements. By having a functions with simlilar
- * names and actions, I hope to simplify porting of applications.
- *
- * A large portion of the functionality of this package can be accessed using a single object,
- * [[scilube.Matlib]]. The usage will be obvious to anyone who's worked in Matlab. Many functions,
- * rather than accepting 2D matrices take 1-D `Array[Double]` instead.
- */
+  * '''scilube-core''' - Science libraries for Scala.
+  *
+  * The scilube-core package contains the base math functions for Scilube. These functions are loosely
+  * modeled after [[http://www.mathworks.com/products/matlab/ Matlab's]] functions. Why Matlab? For
+  * many science applications, the code is prototyped in Matlab and then ported to other languages
+  * for efficiency reasons or to satisfy deployment requirements. By having a functions with simlilar
+  * names and actions, I hope to simplify porting of applications.
+  *
+  * A large portion of the functionality of this package can be accessed using a single object,
+  * [[scilube.Matlib]]. The usage will be obvious to anyone who's worked in Matlab. Many functions,
+  * rather than accepting 2D matrices take 1-D `Array[Double]` instead.
+  */
 package object scilube {
 
   /**
-   * Adds function to support simple math with scalar values
-   * @param a
-   */
+    * Adds function to support simple math with scalar values
+    * @param a
+    */
   implicit class RichArray(a: Array[Double]) {
 
     def +[@specialized(Int, Long, Float, Double) T: Numeric](s: Array[T]): Array[Double] = {
@@ -44,7 +44,7 @@ package object scilube {
 
     def subset(idx: Seq[Int]): Array[Double] = Matlib.subset(a, idx)
 
-    def find(fn: Double => Boolean): Array[Int] = Matlib.find(a, fn).toArray
+    def findIdx(fn: Double => Boolean): Array[Int] = Matlib.find(a, fn).toArray
 
   }
 
